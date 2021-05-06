3c9b354519a757a23502edbd924137e007cf88b6
Lamiel01 <mariovlv80@gmail.com>
2021-05-05 20:10:41 -0500

    New functions

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from functions import *
 
 app = Flask(__name__)
 
@@ -6,7 +7,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/', methods=['GETUSERNAME'])
+@app.route('/', methods=['POST', 'GET'])
 def getUsername():
     text = request.form['text']
     processed_text = text.trim()
--- functions.py
+++ functions.py
@@ -1,13 +1,50 @@
+LICHESS_TOKEN = 'YOUR TOKEN'
 import berserk
 
 session = berserk.TokenSession(LICHESS_TOKEN)
 client = berserk.Client(session=session)
 
-name = 'test'
+name = 'Lamiel01'
+#name = 'P4ssion'
 
-def getBasicInfo(name):
+def test(name) -> None:
+    print(client.users.get_public_data(name))
+
+def getBasicInfo(name) -> tuple:
     info = client.users.get_public_data(name)
     ID = info["id"]
     Username = info['username']
     Active = info['online']
     return ID, Username, Active
+
+def getBasicsStats(name) -> tuple:
+    info = client.users.get_public_data(name)
+    BLITZ = info['perfs']['blitz']['rating']
+    RAPID = info['perfs']['rapid']['rating']
+    PUZZLE = info['perfs']['puzzle']['rating']
+    return BLITZ, RAPID, PUZZLE
+
+def blitz(name) -> tuple:
+    info = client.users.get_public_data(name)
+    RATING = info['perfs']['blitz']['rating']
+    GAMES = info['perfs']['blitz']['games']
+    return RATING, GAMES
+
+def bullet(name) -> tuple:
+    info = client.users.get_public_data(name)
+    RATING = info['perfs']['bullet']['rating']
+    GAMES = info['perfs']['bullet']['games']
+    return RATING, GAMES
+
+def rapid(name) -> tuple:
+    info = client.users.get_public_data(name)
+    RATING = info['perfs']['rapid']['rating']
+    GAMES = info['perfs']['rapid']['games']
+    return RATING, GAMES
+
+#print(f'The basic info of {name} is: BLITZ  RAPID PUZZLE')
+#print(getBasicInfo(name))
+print(f'El jugador {name} tiene en blitz: (rating, partidas jugadas) ' ,blitz(name))
+print(f'El jugador {name} tiene en rapid: (rating, partidas jugadas) ' ,rapid(name))
+print(f'El jugador {name} tiene en bullet: (rating, partidas jugadas) ' ,bullet(name))
+#print(getBasicsStats(name))
